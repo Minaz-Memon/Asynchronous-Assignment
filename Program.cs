ffed4b0fb25fe61cc207d55dcb5740d4cba75655
Minaz Memon <minaz@promactinfo.com>
2022-01-21 04:33:44 +0530

    All the files have been added of Assignment

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+var path="C:/Promact/Training/C-Sharp/AsyncAssgn/A.txt";
+
+using var fs = new FileStream(path,FileMode.Open, FileAccess.Read);
+using var sr = new StreamReader(fs, Encoding.UTF8);
+
+string content = await sr.ReadToEndAsync();
+System.Console.WriteLine(content);
+
+var path1 = "C:/Promact/Training/C-Sharp/AsyncAssgn/B.txt";
+await File.WriteAllTextAsync(path1,content);
+System.Console.WriteLine(File.ReadAllText("B.txt"));
